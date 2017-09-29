23e167b6ad48655d80950f4045da3840e41bedd0
agenis <marc.agenis@gmail.com>
2017-09-29 15:33:45 +0000

    Upload New File

---
+++ script - packages.R
@@ -0,0 +1,39 @@
+#############################
+# ETUDE OPEN DATA PARIS PARCMETRES
+###########################################
+
+# auteur: marc.agenis@gmail.com
+# dernières mise à jour: 29/09/2017
+# PACKAGES
+
+# manipulation de données
+library(tidyverse)
+library(magrittr)
+library(lubridate)
+library(zoo)
+library(xts)
+
+# modelisation
+library(leaps)
+library(ineq)
+library(forecast)
+
+# méteo
+library(weatherData)
+
+# analyse spatiale
+library(rgdal)
+library(spdep)
+library(sp)
+library(gstat)
+library(ape)
+
+# plots
+library(ggplot2)
+library(RColorBrewer)
+library(gridExtra)
+library(grid)
+library(png)
+library(animation)
+library(scales)
+
